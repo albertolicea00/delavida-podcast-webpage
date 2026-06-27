7ebf3cc69b79d7e115325f90774ecb6d92c3c862
albertolicea00 <albertolicea00@icloud.com>
2026-06-27 17:31:56 -0400

    fix: change output configuration from hybrid to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  output: "hybrid",
+  output: "static",
   integrations: [tailwind()],
   i18n: {
     defaultLocale: "es",
